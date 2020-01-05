aaf77525510750800cb4644a4d53adb4cf4ade49
Zoi Dermentzoglou <zoilucky13@yahoo.com>
2020-01-05 11:43:25 +0200

    Lucky

--- EventsProject/EventsProject/login.Designer.cs
+++ EventsProject/EventsProject/login.Designer.cs
@@ -124,6 +124,7 @@
             this.pictureBox1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.ErrorImage = null;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.InitialImage = null;
             this.pictureBox1.Location = new System.Drawing.Point(179, 80);
