82a2e1dc2569e5825761d81195be9c2ef4eb1e90
Κώστας Μπατζιάκας <kmpatziakas@yahoo.gr>
2020-01-09 20:35:18 +0200

    photo

--- EventsProject/EventsProject/Welcome.Designer.cs
+++ EventsProject/EventsProject/Welcome.Designer.cs
@@ -28,52 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Welcome));
-            this.Press_button = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // Press_button
-            // 
-            this.Press_button.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.Press_button.Font = new System.Drawing.Font("Corbel", 10F, System.Drawing.FontStyle.Bold);
-            this.Press_button.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.Press_button.Location = new System.Drawing.Point(110, 348);
-            this.Press_button.Name = "Press_button";
-            this.Press_button.Size = new System.Drawing.Size(196, 33);
-            this.Press_button.TabIndex = 1;
-            this.Press_button.Text = "Press to continue";
-            this.Press_button.UseVisualStyleBackColor = false;
-            this.Press_button.Click += new System.EventHandler(this.Press_button_Click);
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(46, 26);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(324, 256);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
-            // 
-            // Welcome
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.DimGray;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(418, 450);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.Press_button);
-            this.Name = "Welcome";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Welcome";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.ResumeLayout(false);
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Welcome));
+			this.Press_button = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// Press_button
+			// 
+			this.Press_button.BackColor = System.Drawing.SystemColors.MenuHighlight;
+			this.Press_button.Font = new System.Drawing.Font("Corbel", 10F, System.Drawing.FontStyle.Bold);
+			this.Press_button.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.Press_button.Location = new System.Drawing.Point(110, 348);
+			this.Press_button.Name = "Press_button";
+			this.Press_button.Size = new System.Drawing.Size(196, 33);
+			this.Press_button.TabIndex = 1;
+			this.Press_button.Text = "Press to continue";
+			this.Press_button.UseVisualStyleBackColor = false;
+			this.Press_button.Click += new System.EventHandler(this.Press_button_Click);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+			this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(46, 26);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(324, 256);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 2;
+			this.pictureBox1.TabStop = false;
+			// 
+			// Welcome
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.DimGray;
+			this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.ClientSize = new System.Drawing.Size(418, 450);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.Press_button);
+			this.Name = "Welcome";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Welcome";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
 
         }
 
