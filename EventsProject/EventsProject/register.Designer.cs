ac8496dd38c76bedf37f4257d101145f1ad9631b
Andreas <Andreas@DESKTOP-1RTCRTC>
2020-01-09 23:29:48 +0200

    datagridview

--- EventsProject/EventsProject/register.Designer.cs
+++ EventsProject/EventsProject/register.Designer.cs
@@ -194,10 +194,11 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(318, 229);
+            this.dataGridView1.Location = new System.Drawing.Point(299, 242);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(451, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(0, 0);
             this.dataGridView1.TabIndex = 17;
+            this.dataGridView1.Visible = false;
             // 
             // mailTextBox
             // 
